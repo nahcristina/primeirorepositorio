ae36b0dd4fb634d5680c2479e4a2f8cc79713af5
nahcristina <91229601+nahcristina@users.noreply.github.com>
2021-09-22 21:11:34 -0300

    Inicializar o repositório com o arquivo index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Primeiro repositório</title>
+  </head>
+  <body>
+    <h1>
+    <?php
+      echo "Esse é meu primeiro código em repositório";
+    ?>
+    </h1>
+  </body>
+</html>
