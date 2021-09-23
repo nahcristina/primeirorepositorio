6b6d985841e33dcea7fd46e110d436003a21ee65
nahcristina <91229601+nahcristina@users.noreply.github.com>
2021-09-22 22:18:56 -0300

    Iniciando o repositório online

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
-<html lang="pt-br">
   <head>
     <title>Primeiro repositório</title>
   </head>
   <body>
     <h1>
-    <?php
-      echo "Esse é meu primeiro código em repositório";
-    ?>
-    </h1>
+    <?= "Esse é meu primeiro projeto na web"; ?></h1>
   </body>
 </html>
